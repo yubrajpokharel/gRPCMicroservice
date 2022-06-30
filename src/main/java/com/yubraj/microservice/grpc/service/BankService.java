64600c1f399e890e7bf0b7b2245810ce249a07af
Yubraj Pokharel <yubraj.pokharel555@gmail.com>
2022-06-30 01:27:57 -0500

    implement server side streaming

--- src/main/java/com/yubraj/microservice/grpc/service/BankService.java
+++ src/main/java/com/yubraj/microservice/grpc/service/BankService.java
@@ -1,10 +1,9 @@
 package com.yubraj.microservice.grpc.service;
 
 import com.yubraj.microservice.grpc.database.AccountDatabase;
-import com.yubraj.microservice.models.grpc.Balance;
-import com.yubraj.microservice.models.grpc.BankCheckRequest;
-import com.yubraj.microservice.models.grpc.BankServiceGrpc;
+import com.yubraj.microservice.models.grpc.*;
 import com.yubraj.microservice.models.grpc.BankServiceGrpc.BankServiceImplBase;
+import io.grpc.Status;
 import io.grpc.stub.StreamObserver;
 
 public class BankService extends BankServiceImplBase
@@ -14,6 +13,27 @@ public class BankService extends BankServiceImplBase
         int accountNumber = request.getAccountNumber();
         Balance balance = Balance.newBuilder().setAmount(AccountDatabase.getBalance(accountNumber)).build();
         responseObserver.onNext(balance);
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void withDraw(WithdrawRequest request, StreamObserver<Money> responseObserver) {
+        int accNumber = request.getAccountNumber();
+        int amount = request.getAmount();
+        int balance = AccountDatabase.getBalance(accNumber);
+
+        if(balance < amount) {
+            Status status = Status.FAILED_PRECONDITION.withDescription("Not enough money in acc, remaining balance is " + balance);
+            responseObserver.onError(status.asException());
+            return;
+        }
+
+        for (int i = 0; i < amount/10; i++) {
+            Money money = Money.newBuilder().setValue(10).build();
+            responseObserver.onNext(money);
+            AccountDatabase.deductBalance(accNumber, 10);
+        }
+
         responseObserver.onCompleted();
     }
 }
--- src/test/java/com/yubraj/microservice/grpc/BankServiceTest.java
+++ src/test/java/com/yubraj/microservice/grpc/BankServiceTest.java
@@ -1,19 +1,26 @@
 package com.yubraj.microservice.grpc;
 
 
+import com.google.common.util.concurrent.Uninterruptibles;
+import com.yubraj.microservice.grpc.response.MoneyStreamingResponse;
+import com.yubraj.microservice.grpc.service.BankService;
 import com.yubraj.microservice.models.grpc.Balance;
 import com.yubraj.microservice.models.grpc.BankCheckRequest;
 import com.yubraj.microservice.models.grpc.BankServiceGrpc;
+import com.yubraj.microservice.models.grpc.WithdrawRequest;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
+import java.util.concurrent.TimeUnit;
+
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class BankServiceTest {
 
     BankServiceGrpc.BankServiceBlockingStub serviceBlockingStub;
+    BankServiceGrpc.BankServiceStub bankServiceStub;
 
     @BeforeAll
     public void setUp() {
@@ -22,6 +29,7 @@ public class BankServiceTest {
                 .build();
 
         this.serviceBlockingStub = BankServiceGrpc.newBlockingStub(channel);
+        this.bankServiceStub = BankServiceGrpc.newStub(channel);
     }
 
 
@@ -31,6 +39,23 @@ public class BankServiceTest {
         BankCheckRequest request = BankCheckRequest.newBuilder().setAccountNumber(10).build();
         Balance balance = this.serviceBlockingStub.getBalance(request);
         System.out.println(balance);
+    }
+
+    @Test
+    public void withDrawTest() {
+        WithdrawRequest withdrawRequest = WithdrawRequest.newBuilder().setAccountNumber(9).setAmount(50).build();
+        serviceBlockingStub.withDraw(withdrawRequest).forEachRemaining(
+                money -> {
+                    System.out.println(money.getValue());
+                }
+        );
+    }
+
+    @Test
+    public void withDrawTestAsync() {
+        WithdrawRequest withdrawRequest = WithdrawRequest.newBuilder().setAccountNumber(11).setAmount(50).build();
+        bankServiceStub.withDraw(withdrawRequest, new MoneyStreamingResponse());
+        Uninterruptibles.sleepUninterruptibly(3, TimeUnit.SECONDS);
     }
 
 }
