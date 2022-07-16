136f9553d06619ef906bbc68afc290fb8bf6cd29
Yubraj Pokharel <yubraj.pokharel555@gmail.com>
2022-07-16 01:51:19 -0500

    load balancing test

--- src/test/java/com/yubraj/microservice/grpc/TransferClientTest.java
+++ src/test/java/com/yubraj/microservice/grpc/TransferClientTest.java
@@ -1,4 +1,56 @@
 package com.yubraj.microservice.grpc;
 
+import com.yubraj.microservice.grpc.request.TransferStreamingResponse;
+import com.yubraj.microservice.grpc.service.TransferService;
+import com.yubraj.microservice.models.grpc.BankServiceGrpc;
+import com.yubraj.microservice.models.grpc.TransferRequest;
+import com.yubraj.microservice.models.grpc.TransferServiceGrpc;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.stub.StreamObserver;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ThreadLocalRandom;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class TransferClientTest {
+
+    TransferServiceGrpc.TransferServiceStub transferServiceStub;
+
+    @BeforeAll
+    public void setUp() {
+        ManagedChannel channel = ManagedChannelBuilder
+                .forAddress("localhost", 6565)
+                .usePlaintext()
+                .build();
+
+        this.transferServiceStub = TransferServiceGrpc.newStub(channel);
+    }
+
+
+    @Test
+    public void transfer() throws InterruptedException {
+        CountDownLatch latch = new CountDownLatch(1);
+        TransferStreamingResponse response = new TransferStreamingResponse(latch);
+        StreamObserver<TransferRequest> requestStreamObserver = this.transferServiceStub
+                .transfer(response);
+
+        for (int i = 0; i < 100; i++) {
+            TransferRequest transferRequest = TransferRequest.newBuilder()
+                    .setFromAccount(ThreadLocalRandom.current().nextInt(1, 11))
+                    .setToAccount(ThreadLocalRandom.current().nextInt(1, 11))
+                    .setAmount(ThreadLocalRandom.current().nextInt(1, 11))
+                    .build();
+
+            requestStreamObserver.onNext(transferRequest);
+        }
+
+        requestStreamObserver.onCompleted();
+        latch.await();
+    }
+
+
 }
